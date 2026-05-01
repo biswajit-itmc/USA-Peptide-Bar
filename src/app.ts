fac6814c94d9245e423496dcf10e21f9d577fdfd
T490 <biswajit.rath@itmcsystems.com>
2026-05-01 20:30:31 +0530

    updated cors

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ export const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"], 
+  origin: ["https://usapeptidebar.com/"], 
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true
 }));
