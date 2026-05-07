7d46fc0986b8894157a121e8bc421d2d20a8e65f
jitendra Kumar singh <Jitendra.K.Singh@itmcsystems.com>
2026-05-07 19:13:37 +0530

    Updated documents and edit profile pages upto logout

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+
 
 /* ✅ BODY SIZE FIX (413 ERROR SOLVE) */
 app.use(express.json({ limit: "50mb" }));
